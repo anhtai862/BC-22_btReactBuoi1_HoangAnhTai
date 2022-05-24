45eba9b2d9e79e7cea209f3d2a981255e7f1b7e7
anhtai862 <leovince862@gmail.com>
2022-05-24 18:34:06 +0700

    upload bt react b1

---
+++ BT-ReactB1/src/App.js
@@ -0,0 +1,10 @@
+import LayoutMSI from "./1_Layout-MSI/LayoutMSI";
+import "./App.css";
+
+function App() {
+  const title = "Layout MSI";
+  // JSX : Javascript XML - cho phép viét HTML trong javascript
+  return <LayoutMSI />;
+}
+
+export default App;
